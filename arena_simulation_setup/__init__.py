1923cabf90d7d5c70a7521b60fbb95dd9d31937f
Vova Shcherbyna <dev@voshch.dev>
2025-05-07 19:28:56 +0200

    extend robot api with model.params frames

--- arena_simulation_setup/__init__.py
+++ arena_simulation_setup/__init__.py
@@ -1,6 +1,8 @@
 import os
 import typing
+
 import ament_index_python.packages
+import yaml
 
 ass_dir = ament_index_python.packages.get_package_share_directory('arena_simulation_setup')
 
@@ -68,12 +70,31 @@ class World(_AssInterface):
 class Robot(_AssInterface):
     _base_dir = os.path.join(_AssInterface._base_dir, 'entities', 'robots')
 
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self._cached_params = None
+
+    @property
+    def _model_params(self) -> dict[str, typing.Any]:
+        if self._cached_params is None:
+            with open(os.path.join(self.dir, 'model_params.yaml')) as f:
+                self._cached_params = yaml.safe_load(f)
+        return self._cached_params
+
     @property
     def mappings(self) -> str:
         return os.path.join(
             self.dir,
             'mappings.yaml'
         )
+
+    @property
+    def base_frame(self) -> str:
+        return self._model_params.get('robot_base_frame', 'base_link')
+
+    @property
+    def odom_frame(self) -> str:
+        return self._model_params.get('robot_odom_frame', 'odom')
 
 
 class Obstacle(_AssInterface):
