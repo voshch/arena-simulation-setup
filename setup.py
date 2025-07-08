7caba7dc2dd675d01037baee7027a5b58ab577cc
Vova Shcherbyna <dev@voshch.dev>
2025-07-08 14:42:12 +0200

    fix merge

--- setup.py
+++ setup.py
@@ -40,11 +40,15 @@ setup(
     description='arena_simulation_setup.',
     license='BSD',
     tests_require=['pytest'],
-    scripts=[],
+    scripts=[
+        'scripts/generate_world',
+        'scripts/model_staging',
+    ],
     entry_points={
         'console_scripts': [
             'generate_world = arena_simulation_setup.utils.generative.world_generator:main',
             'world_generator = arena_simulation_setup.utils.generative.world_generator_ros:main',
+            'model_staging = arena_simulation_setup.model_staging:main'
         ],
     },
 )
